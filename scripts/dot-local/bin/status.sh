c976b1895a4b252f21394a3b3226fffd1f3b891e
Chris Kiriakou <chris@kiriakou.net>
2026-08-19 21:36:42 +0200

    remove wifi as it not reproducible

--- scripts/dot-local/bin/status.sh
+++ scripts/dot-local/bin/status.sh
@@ -3,13 +3,12 @@
 # Fetch status information
 
 main() {
-    wifi="Wifi: $(nmcli --terse --colors no connection show --active | sed 2d | cut --delimiter ':' --fields=1) | "
     uptime="$(uptime --pretty) | "
     disk="Disk: $(df --human-readable / --output=pcent | sed 1d | cut --whitespace-delimited --fields=2) | "
     ram="RAM: $(free --human --line | cut --whitespace-delimited --fields=4) | "
     date="$(date +'%a, %d.%m.%Y %H:%M') "
     test -d /sys/class/power_supply/CMB1 && bat="Bat: $(~/.local/bin/batterystate.sh) | "
-    printf "%s\n" "${uptime}${disk}${ram}${bat}${wifi}${date}"
+    printf "%s\n" "${uptime}${disk}${ram}${bat}${date}"
 }
 
 main
