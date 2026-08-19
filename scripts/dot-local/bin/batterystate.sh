7001af963934b65ab55f24eae2c7570cd6e59a4a
Chris Kiriakou <chris@kiriakou.net>
2026-08-19 17:13:04 +0200

    make script oneshot instead of loop

--- scripts/dot-local/bin/batterystate.sh
+++ scripts/dot-local/bin/batterystate.sh
@@ -1,21 +1,16 @@
 #!/bin/sh
 #
-# Check the system status
+# Check the battery status
 
-get_battery() {
-    read bat_perc < /sys/class/power_supply/BAT1/capacity
-    read bat_status < /sys/class/power_supply/BAT1/status
+bat_name="CMB1"
+
+main() {
+    read bat_perc < /sys/class/power_supply/${bat_name}/capacity
+    read bat_status < /sys/class/power_supply/${bat_name}/status
     # Send warning if battery is low and no charger is plugged in
     test "${bat_perc}" -le 5 && test "${bat_status}" != 'Charging' \
         && notify-send --urgency=ciritcal "Plug in a charger, low battery!"
     printf "${bat_perc}-${bat_status}"
-}
-
-main() {
-    while true; do
-      get_battery
-      sleep 60
-    done
 }
 
 main
