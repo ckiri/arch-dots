ae43724420c168ac7c324806288f7024a6b4637e
Chris Kiriakou <chris@kiriakou.net>
2026-08-19 22:10:51 +0200

    move battery check to status script

--- scripts/dot-local/bin/batterystate.sh
+++ scripts/dot-local/bin/batterystate.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 #
-# Check the battery status
-
+# Check the battery status; provide its battery name (e.g. BAT0)
 
 main() {
-    bat_name=$(ls --format=single-column /sys/class/power_supply/ | grep -E '\b[A-Z]{2,3}[0-9]\b')
-    test ! -n "$bat_name" && exit 1
-    read bat_perc < /sys/class/power_supply/${bat_name}/capacity
-    read bat_status < /sys/class/power_supply/${bat_name}/status
+    test ! -d /sys/class/power_supply/${1} && exit 1
+    read bat_perc < /sys/class/power_supply/${1}/capacity
+    read bat_status < /sys/class/power_supply/${1}/status
     # Send warning if battery is low and no charger is plugged in
     test "${bat_perc}" -le 5 && test "${bat_status}" != 'Charging' \
         && notify-send --urgency=ciritcal "Plug in a charger, low battery!"
     printf "%s\n" "${bat_perc}% (${bat_status})"
 }
 
-main
-
+main $1
--- scripts/dot-local/bin/status.sh
+++ scripts/dot-local/bin/status.sh
@@ -7,8 +7,11 @@ main() {
     disk="Disk: $(df --human-readable / --output=pcent | sed 1d | cut --whitespace-delimited --fields=2) | "
     ram="RAM: $(free --human --line | cut --whitespace-delimited --fields=4) | "
     date="$(date +'%a, %d.%m.%Y %H:%M') "
-    bat="Bat: $(~/.local/bin/batterystate.sh) | "
+    bat_name=$(ls --format=single-column /sys/class/power_supply/ | grep -E '\b[A-Z]{2,3}[0-9]\b')
+    test -n "${bat_name}" && bat="Bat: $(~/.local/bin/batterystate.sh ${bat_name}) | "
     printf "%s\n" "${uptime}${disk}${ram}${bat}${date}"
 }
 
 main
+
+
